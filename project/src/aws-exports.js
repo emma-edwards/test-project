95d8579be55e40285790372485728afa4a458c8e
emma-edwards <eee946@uowmail.edu.au>
2022-12-12 18:28:49 +1100

    add db and notes function

--- project/src/aws-exports.js
+++ project/src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://mvxyriotanfhpgve4hwjm7l4em.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-southeast-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-exzay5khurde7o7tbhbtafqupe"
 };
 
 
